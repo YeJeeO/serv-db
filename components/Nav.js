4a13ed36e5d193d24c57ae40b56509e8b842bb2c
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-08-17 22:43:17 +0400

    Nav.js поправил

--- components/Nav.js
+++ components/Nav.js
@@ -3,45 +3,45 @@ import * as React from 'react';
 import { Container, Card, Row, Text } from "@nextui-org/react";
 import { Link } from "@nextui-org/react";
 import NextLink from "next/link";
-import { Navbar, Avatar } from "@nextui-org/react";
+import { Navbar, Avatar, NavbarBrand, NavbarContent } from "@nextui-org/react";
 
 
 const preventDefault = (event) => event.preventDefault();
 
 export default function Nav() {
-const [variant, setVariant] = React.useState("static");
-const variants = ["floating"];
-  return (
-    
-    <Container
-          onClick={preventDefault}
-    >
- 
- <Navbar isBordered variant={variant} >
- 
-        <Navbar.Content css={{
-      maxW: "100%",
-      boxSizing: "border-box",
-      dflex: "center",
-      position: "fixed",
-      width: "100%",}}>
-        <Navbar.Brand> 
-        <Avatar
-          size="xl"
-          src="./logo_mini.jpg"
-          color="gradient"
-          bordered
-          squared
-        />
-        </Navbar.Brand>
+  const [variant, setVariant] = React.useState("static");
+  const variants = ["floating"];
+  return (<>
+
+    {/* <Container onClick={preventDefault}> */}
+
+    <Navbar isBordered variant={variant} >
+
+      <NavbarContent css={{
+        maxW: "100%",
+        boxSizing: "border-box",
+        dflex: "center",
+        position: "fixed",
+        width: "100%"
+      }}>
+        <NavbarBrand>
+          <Avatar
+            size="xl"
+            src="./logo_mini.jpg"
+            color="gradient"
+            bordered
+            squared
+          />
+        </NavbarBrand>
         <Link as={NextLink} href='/' >О нас</Link>
-        <Link as={NextLink}  href='/second'>Преподаватели</Link>
-        <Link as={NextLink}  href='/third'>Мероприятия</Link>
-        <Link as={NextLink}  href='/fouth'>Галерея</Link>
-        <Link as={NextLink}  href='/fifth'>Расписание</Link>
-        <Link as={NextLink}  href='/admin'>Admin</Link>
-        </Navbar.Content>
-        </Navbar>
-       </Container>
+        <Link as={NextLink} href='/second'>Преподаватели</Link>
+        <Link as={NextLink} href='/third'>Мероприятия</Link>
+        <Link as={NextLink} href='/fouth'>Галерея</Link>
+        <Link as={NextLink} href='/fifth'>Расписание</Link>
+        <Link as={NextLink} href='/admin'>Admin</Link>
+      </NavbarContent>
+    </Navbar>
+    {/*     </Container> */}
+  </>
   );
 }
